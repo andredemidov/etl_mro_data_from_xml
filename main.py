a6b4112aae5e43ccadb29170a32665d5c25d9247
andredemidov <demidov-ai@yandex.ru>
2023-03-01 15:32:50 +0400

    logging added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+from datetime import datetime
 from domain import use_cases
 import data_sources
 from repositories import Repository
@@ -6,7 +8,25 @@ URL = 'https://operation.irkutskoil.ru/'
 XML_FILE = 'test_data/test_toir_data.xml'
 
 
+def log_statistic(statistic: dict):
+    message = ', '.join([f'{c[0]} - {c[1]}' for c in statistic.items()])
+    if statistic.get('error'):
+        logging.warning(message)
+    else:
+        logging.info(message)
+
+
 def start():
+    logs_path = 'c://python/logs/'
+    logging.basicConfig(
+        format='%(asctime)s : %(levelname)s : %(message)s',
+        level=logging.INFO,
+        handlers=[
+            logging.FileHandler(logs_path + datetime.now().strftime("%Y-%m-%d") + f'_toir.log'),
+            logging.StreamHandler()
+        ]
+    )
+
     get_new_data_adapter = data_sources.GetNewDataAdapter(file_path=XML_FILE)
 
     with open('auth_data.txt') as f:
@@ -17,6 +37,7 @@ def start():
     post_data_adapter = data_sources.PostDataAdapter(url=URL, token=token)
 
     use_cases.GetOperationObjects(get_current_data_adapter, operation_object_repository).execute()
+    logging.info(f'Total objects {len(operation_object_repository.list())}')
 
     for operation_object in operation_object_repository.list():
 
@@ -33,6 +54,12 @@ def start():
 
         new_equipment_repository = Repository()
         use_cases.GetNewEquipment(get_new_data_adapter, new_equipment_repository, operation_object).execute()
+
+        logging.info(
+            f'groups: {len(new_object_repair_group_repository.list())}',
+            f'tech_positions: {len(new_tech_position_repository.list())}',
+            f'equipments: {len(new_equipment_repository.list())}',
+        )
 
         use_cases.SetParentReference(
             operation_object=operation_object,
@@ -51,8 +78,9 @@ def start():
             current_entities_repository=current_object_repair_group_repository,
         ).execute()
 
+        logging.info('creating and updating of groups')
         statistic = use_cases.SaveEntities(post_data_adapter, new_object_repair_group_repository).execute()
-        print(statistic)
+        log_statistic(statistic)
 
         # tech_position
         current_tech_position_repository = Repository()
@@ -65,8 +93,9 @@ def start():
             current_entities_repository=current_tech_position_repository,
         ).execute()
 
+        logging.info('creating and updating of tech_positions')
         statistic = use_cases.SaveEntities(post_data_adapter, new_tech_position_repository).execute()
-        print(statistic)
+        log_statistic(statistic)
 
         # equipment
         current_equipment_repository = Repository()
@@ -79,17 +108,22 @@ def start():
             current_entities_repository=current_equipment_repository,
         ).execute()
 
+        logging.info('creating and updating of equipments')
         statistic = use_cases.SaveEntities(post_data_adapter, new_equipment_repository).execute()
-        print(statistic)
+        log_statistic(statistic)
 
         # delete
-
+        logging.info('deleting of groups')
         statistic = use_cases.DeleteEntities(post_data_adapter, new_object_repair_group_repository).execute()
-        print('object_repair_group', statistic)
+        log_statistic(statistic)
+
+        logging.info('deleting of tech_positions')
         statistic = use_cases.DeleteEntities(post_data_adapter, new_tech_position_repository).execute()
-        print('tech_position', statistic)
+        log_statistic(statistic)
+
+        logging.info('deleting of equipments')
         statistic = use_cases.DeleteEntities(post_data_adapter, new_equipment_repository).execute()
-        print('equipment', statistic)
+        log_statistic(statistic)
 
 
 if __name__ == '__main__':
