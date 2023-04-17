fb69b0cf0b84c472be319e0a667f77b60f3e9805
andredemidov <demidov-ai@yandex.ru>
2023-04-17 09:47:47 +0400

    refactoring

--- main.py
+++ main.py
@@ -16,8 +16,50 @@ def log_statistic(statistic: dict):
         logging.info(message)
 
 
-def start():
-    logs_path = 'c://python/logs/'
+def integrate_operation_object(operation_object, get_new_data_adapter, get_current_data_adapter, post_data_adapter):
+    # new data
+    new_repository = repositories.NewObjectsRepository(
+        operation_object=operation_object,
+        get_new_data_adapter=get_new_data_adapter,
+        post_data_adapter=post_data_adapter
+    )
+    logging.info(f'new entities: {len(new_repository.get())}')
+
+    transformations.SetParentReference(operation_object=operation_object,
+                                       repository=new_repository).execute()
+
+    # current data
+    current_repository = repositories.CurrentObjectsRepository(operation_object, get_current_data_adapter)
+
+    transformations.MatchNewAndCurrentEntities(
+        new_entities_repository=new_repository,
+        current_entities_repository=current_repository,
+    ).execute()
+    statuses = list(map(lambda x: x.update_status, new_repository.get()))
+    counter = Counter(statuses)
+    logging.info(f'statuses: {counter}')
+
+    logging.info('creating and updating')
+    statistic = new_repository.save()
+    log_statistic(statistic)
+
+    logging.info('creating and updating nested objects')
+    statistic = new_repository.save_nested_objects()
+    log_statistic(statistic)
+
+    # delete
+    logging.info('deleting')
+    statistic = new_repository.delete()
+    log_statistic(statistic)
+
+    logging.info('deleting nested objects')
+    statistic = new_repository.delete_nested_objects()
+    log_statistic(statistic)
+
+
+def init_log(logs_path: str = None):
+    if not logs_path:
+        logs_path = 'c://python/logs/'
     logging.basicConfig(
         format='%(asctime)s : %(levelname)s : %(message)s',
         level=logging.INFO,
@@ -27,11 +69,19 @@ def start():
         ]
     )
 
-    get_new_data_adapter = data_sources.GetNewDataAdapter(file_directory=XML_FILE_DIRECTORY)
 
-    with open('auth_data.txt') as f:
+def init_neosintez_session(url, auth_data_file_path: str = None):
+    if not auth_data_file_path:
+        auth_data_file_path = 'auth_data.txt'
+    with open(auth_data_file_path) as f:
         aut_string = f.read()
-    session = data_sources.GetSession.execute(URL, aut_string)
+    return data_sources.GetSession.execute(url, aut_string)
+
+
+if __name__ == '__main__':
+    init_log()
+    get_new_data_adapter = data_sources.GetNewDataAdapter(file_directory=XML_FILE_DIRECTORY)
+    session = init_neosintez_session(URL)
     try:
         get_current_data_adapter = data_sources.GetCurrentDataAdapter(url=URL, session=session)
         post_data_adapter = data_sources.PostDataAdapter(url=URL, session=session)
@@ -40,44 +90,12 @@ def start():
 
         for operation_object in operation_object_repository.get():
             try:
-                # new data
-                new_repository = repositories.NewObjectsRepository(
-                    operation_object=operation_object,
-                    get_new_data_adapter=get_new_data_adapter,
-                    post_data_adapter=post_data_adapter
+                integrate_operation_object(
+                    operation_object,
+                    get_new_data_adapter,
+                    get_current_data_adapter,
+                    post_data_adapter
                 )
-                logging.info(f'new entities: {len(new_repository.get())}')
-
-                transformations.SetParentReference(operation_object=operation_object,
-                                                   repository=new_repository).execute()
-
-                # current data
-                current_repository = repositories.CurrentObjectsRepository(operation_object, get_current_data_adapter)
-
-                transformations.MatchNewAndCurrentEntities(
-                    new_entities_repository=new_repository,
-                    current_entities_repository=current_repository,
-                ).execute()
-                statuses = list(map(lambda x: x.update_status, new_repository.get()))
-                counter = Counter(statuses)
-                logging.info(f'statuses: {counter}')
-
-                logging.info('creating and updating')
-                statistic = new_repository.save()
-                log_statistic(statistic)
-
-                logging.info('creating and updating nested objects')
-                statistic = new_repository.save_nested_objects()
-                log_statistic(statistic)
-
-                # delete
-                logging.info('deleting')
-                statistic = new_repository.delete()
-                log_statistic(statistic)
-
-                logging.info('deleting nested objects')
-                statistic = new_repository.delete_nested_objects()
-                log_statistic(statistic)
 
             except Exception as e:
                 print(e)
@@ -85,7 +103,3 @@ def start():
     finally:
         session.close()
         logging.info('Session is closed')
-
-
-if __name__ == '__main__':
-    start()
